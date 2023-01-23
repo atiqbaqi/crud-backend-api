da1db2067c23df677b4d834d3c98b1508cb428c1
Atiqul Hoque <73009994+atiqbaqi@users.noreply.github.com>
2023-01-23 22:32:14 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const app_port=8080;
+
+// Tell Express to use EJS as the template engine
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/api',(req,res)=>{
+    return res.status(200).send('Welcome');
+});
+
+app.listen(app_port,()=>{
+    console.log(`listening on ${app_port}`);
+});
+
