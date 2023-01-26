0c0f4aad2b22210e7f904d5deb807f56c2eb27de
Atiqul Hoque <atiqbaqi@gmail.com>
2023-01-26 16:45:34 +0600

    added helmet to increase the HTTP header security

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 require('dotenv').config();
+const helmet = require('helmet');
 const route = require('./routes/route');
 
 const app_port=process.env.APP_PORT || 8080;
@@ -11,6 +12,17 @@ app.use(morgan('combined'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//increase the HTTP header security usign helmet
+app.use(
+    helmet.contentSecurityPolicy({
+        useDefaults: true,
+        directives: {
+            "script-src": ["'self'", "securecoding.com"],
+            "style-src": null,
+        },
+    })
+  );
 app.use('/api',route);
 
 
