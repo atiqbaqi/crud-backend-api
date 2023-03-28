04ba24caee89a22a1b17f482a63ca7e8d3eea865
Atiqul Hoque <73009994+atiqbaqi@users.noreply.github.com>
2023-03-28 19:55:53 +0600

    db update

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               10.4.24-MariaDB - mariadb.org binary distribution
+-- Server version:               10.4.27-MariaDB - mariadb.org binary distribution
 -- Server OS:                    Win64
--- HeidiSQL Version:             12.3.0.6589
+-- HeidiSQL Version:             12.4.0.6659
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,33 +16,44 @@
 
 
 -- Dumping database structure for crud_app_db
+DROP DATABASE IF EXISTS `crud_app_db`;
 CREATE DATABASE IF NOT EXISTS `crud_app_db` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `crud_app_db`;
 
 -- Dumping structure for table crud_app_db.events
+DROP TABLE IF EXISTS `events`;
 CREATE TABLE IF NOT EXISTS `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `event_name` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `event_location` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `event_name` varchar(500) DEFAULT NULL,
+  `event_location` varchar(50) DEFAULT NULL,
   `event_date` datetime DEFAULT NULL,
+  `created_by` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `updated_by` int(11) NOT NULL,
   `updated_at` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table crud_app_db.events: ~0 rows (approximately)
+-- Dumping data for table crud_app_db.events: ~2 rows (approximately)
+DELETE FROM `events`;
+INSERT INTO `events` (`id`, `event_name`, `event_location`, `event_date`, `created_by`, `created_at`, `updated_by`, `updated_at`) VALUES
+	(1, 'neural-network', 'Russia', '2023-03-15 16:00:00', 0, '2023-03-27 05:25:59', 0, '2023-03-27 05:27:27'),
+	(2, 'data-backup', 'USA', '2023-03-23 12:00:00', 0, '2023-03-27 05:40:17', 0, '2023-03-27 05:40:17'),
+	(3, 'event-1', 'Russia', '2023-10-23 20:10:00', 0, '2023-03-28 11:15:52', 0, '2023-03-28 11:15:52');
 
 -- Dumping structure for table crud_app_db.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table crud_app_db.users: ~1 rows (approximately)
+-- Dumping data for table crud_app_db.users: ~0 rows (approximately)
+DELETE FROM `users`;
 INSERT INTO `users` (`id`, `user_name`, `password`, `created_at`, `updated_at`) VALUES
 	(1, 'api_user', 'api_password', '2023-01-24 05:12:42', '2023-01-24 05:12:42');
 
